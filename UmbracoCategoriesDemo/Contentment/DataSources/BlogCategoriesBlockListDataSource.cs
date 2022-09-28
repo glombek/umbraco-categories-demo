31dc9500680ccfa462ad21e468a624951dac9e59
Joe Glombek <joe@glombek.com>
2022-09-28 21:04:17 +0100

    Config for category selection

---
+++ UmbracoCategoriesDemo/Contentment/DataSources/BlogCategoriesBlockListDataSource.cs
@@ -0,0 +1,32 @@
+﻿using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.PublishedModels;
+using Umbraco.Community.Contentment.DataEditors;
+
+namespace UmbracoCategoriesDemo.Contentment.DataSources
+{
+    public class BlogCategoriesBlockListDataSource : UmbracoContextDataListSource
+    {
+        public BlogCategoriesBlockListDataSource(IRequestAccessor requestAccessor, IUmbracoContextAccessor umbracoContextAccessor) : base(requestAccessor, umbracoContextAccessor) { }
+
+        public override Dictionary<string, object> DefaultValues => new Dictionary<string, object>();
+
+        public override IEnumerable<ConfigurationField> Fields => new ConfigurationField[] { };
+
+        public override OverlaySize OverlaySize => OverlaySize.Small;
+
+        public override string Name => "Blog categories (block list)";
+
+        public override string Description => "Pulls blog categories from the block list on the parent node.";
+
+        public override string Icon => "icon-tags";
+
+        public override IEnumerable<DataListItem> GetItems(UmbracoContext context, Dictionary<string, object> config)
+        {
+            var parent = context?.Content?.Parent as Blog;
+
+            return parent?.CategoriesAsBlockListItems?.Cast<BlockListItem<CategoryAsBlockListItem>>().Select(x => new DataListItem() { Name = x.Content.CategoryName, Value = x.ContentUdi.ToString(), Description = x.Content.Description }) ?? new DataListItem[] { };
+        }
+    }
+}
---
+++ UmbracoCategoriesDemo/Contentment/DataSources/BlogCategoriesRepeatableTextStringDataSource.cs
@@ -0,0 +1,32 @@
+﻿using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.PublishedModels;
+using Umbraco.Community.Contentment.DataEditors;
+
+namespace UmbracoCategoriesDemo.Contentment.DataSources
+{
+    public class BlogCategoriesRepeatableTextStringDataSource : UmbracoContextDataListSource
+    {
+        public BlogCategoriesRepeatableTextStringDataSource(IRequestAccessor requestAccessor, IUmbracoContextAccessor umbracoContextAccessor) : base(requestAccessor, umbracoContextAccessor) { }
+
+        public override Dictionary<string, object> DefaultValues => new Dictionary<string, object>();
+
+        public override IEnumerable<ConfigurationField> Fields => new ConfigurationField[] { };
+
+        public override OverlaySize OverlaySize => OverlaySize.Small;
+
+        public override string Name => "Blog categories (repeatable text strings)";
+
+        public override string Description => "Pulls blog categories from the repeatable text strings on the parent node.";
+
+        public override string Icon => "icon-tags";
+
+        public override IEnumerable<DataListItem> GetItems(UmbracoContext context, Dictionary<string, object> config)
+        {
+            var parent = context?.Content?.Parent as Blog;
+
+            return parent?.CategoriesAsRepeatableTextStrings?.Select(x => new DataListItem() { Name = x, Value = x }) ?? new DataListItem[] { };
+        }
+    }
+}
