a6c94a0380780e34ff78544e916c67fff590d06c
Joe Glombek <joe@glombek.com>
2022-10-09 17:06:01 +0100

    Update to exactly match PR to Contentment

---
+++ UmbracoCategoriesDemo/Contentment/DataSources/BaseUmbracoContentDataListSource.cs
@@ -0,0 +1,81 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.Web;
+
+namespace Umbraco.Community.Contentment.DataEditors
+{
+    public abstract class BaseUmbracoContentDataListSource : IDataListSource
+    {
+        private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+
+        private readonly IRequestAccessor _requestAccessor;
+
+        public BaseUmbracoContentDataListSource(
+            IRequestAccessor requestAccessor,
+            IUmbracoContextAccessor umbracoContextAccessor)
+        {
+            _requestAccessor = requestAccessor;
+            _umbracoContextAccessor = umbracoContextAccessor;
+
+        }
+        
+        protected int? ContextContentId
+        {
+            get
+            {
+                var umbracoContext = _umbracoContextAccessor.GetRequiredUmbracoContext();
+                isContextContentParent = false;
+
+                // NOTE: First we check for "id" (if on a content page), then "parentId" (if editing an element).
+                if (int.TryParse(_requestAccessor.GetQueryStringValue("id"), out var currentId) == true)
+                {
+                    return currentId;
+                }
+                else if (int.TryParse(_requestAccessor.GetQueryStringValue("parentId"), out var parentId) == true)
+                {
+                    isContextContentParent = true;
+                    return parentId;
+                }
+
+                return default(int?);
+            }
+        }
+
+        protected IPublishedContent? GetContextContent()
+        {
+            var umbracoContext = _umbracoContextAccessor.GetRequiredUmbracoContext();
+            if (ContextContentId.HasValue == false || umbracoContext == null)
+            {
+                return null;
+            }
+
+            return umbracoContext?.Content?.GetById(true, ContextContentId.Value);
+        }
+
+        private bool isContextContentParent;
+        protected bool IsContextContentParent
+        {
+            get
+            {
+                return ContextContentId.HasValue && isContextContentParent;
+            }
+        }
+
+
+        public abstract Dictionary<string, object> DefaultValues { get; }
+
+        public abstract IEnumerable<ConfigurationField> Fields { get; }
+
+        public virtual string Group { get; } = "Umbraco";
+
+        public abstract OverlaySize OverlaySize { get; }
+
+        public abstract string Name { get; }
+
+        public abstract string Description { get; }
+
+        public abstract string Icon { get; }
+
+        public abstract IEnumerable<DataListItem> GetItems(Dictionary<string, object> config);
+    }
+}
--- UmbracoCategoriesDemo/Contentment/DataSources/BlogCategoriesBlockListDataSource.cs
+++ UmbracoCategoriesDemo/Contentment/DataSources/BlogCategoriesBlockListDataSource.cs
@@ -1,14 +1,17 @@
 ﻿using Umbraco.Cms.Core.Models.Blocks;
 using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Web.Common.PublishedModels;
 using Umbraco.Community.Contentment.DataEditors;
 
 namespace UmbracoCategoriesDemo.Contentment.DataSources
 {
-    public class BlogCategoriesBlockListDataSource : UmbracoContextDataListSource
+    public class BlogCategoriesBlockListDataSource : BaseUmbracoContentDataListSource
     {
-        public BlogCategoriesBlockListDataSource(IRequestAccessor requestAccessor, IUmbracoContextAccessor umbracoContextAccessor) : base(requestAccessor, umbracoContextAccessor) { }
+        public BlogCategoriesBlockListDataSource(IContentTypeService contentTypeService, IRequestAccessor requestAccessor, IUmbracoContextAccessor umbracoContextAccessor) : base(requestAccessor, umbracoContextAccessor)
+        {
+        }
 
         public override Dictionary<string, object> DefaultValues => new Dictionary<string, object>();
 
@@ -22,9 +25,9 @@ namespace UmbracoCategoriesDemo.Contentment.DataSources
 
         public override string Icon => "icon-tags";
 
-        public override IEnumerable<DataListItem> GetItems(UmbracoContext context, Dictionary<string, object> config)
+        public override IEnumerable<DataListItem> GetItems(Dictionary<string, object> config)
         {
-            var parent = context?.Content?.Parent as Blog;
+            var parent = GetContextContent().AncestorOrSelf<Blog>();
 
             return parent?.CategoriesAsBlockListItems?.Cast<BlockListItem<CategoryAsBlockListItem>>().Select(x => new DataListItem() { Name = x.Content.CategoryName, Value = x.ContentUdi.ToString(), Description = x.Content.Description }) ?? new DataListItem[] { };
         }
